2b4253791096a4782282c128f2e89c82ae56c942
Syed Farzeen Ahmad <syedfarzeen18@gmail.com>
2026-03-07 14:42:36 +0530

    feat(tags): Completed Tag module for Tag creation

---
+++ apps/api/src/modules/tags/tags.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateTagDto } from './dto/create-tag.dto';
+import { Tag } from './tag.entity';
+
+@Injectable()
+export class TagsService {
+  constructor(
+    @InjectRepository(Tag)
+    private readonly tagRepository: Repository<Tag>,
+  ) {}
+
+  async create(createTagDto: CreateTagDto) {
+    const tag = this.tagRepository.create({ name: createTagDto.name.trim() });
+    return this.tagRepository.save(tag);
+  }
+
+  async findAll() {
+    return this.tagRepository.find();
+  }
+}
